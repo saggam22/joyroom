16f39cd8f02150deacd619101b63ae7d065b5f26
Binggla <wlqls12@naver.com>
2022-05-20 02:29:50 +0900

    dao_mac 바꿈

--- caffeineD/src/main/java/co/dev/dao/ReviewDAO.java
+++ caffeineD/src/main/java/co/dev/dao/ReviewDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 import co.dev.vo.ReviewVO;
 import co.dev.vo.UserVO;
 
-public class ReviewDAO extends DAO_mac {
+public class ReviewDAO extends DAO {
 
 	// 리뷰 리스트 조회
 	public List<ReviewVO> selectReviews(int cafeNo) {
