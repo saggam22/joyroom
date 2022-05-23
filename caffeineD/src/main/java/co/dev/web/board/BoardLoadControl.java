313dee73e7031df2e3869b2c075823c3a0084686
hwigle <96683175+hwigle@users.noreply.github.com>
2022-05-24 07:39:50 +0900

    Update BoardLoadControl.java

--- caffeineD/src/main/java/co/dev/web/board/BoardLoadControl.java
+++ caffeineD/src/main/java/co/dev/web/board/BoardLoadControl.java
@@ -28,7 +28,7 @@ public class BoardLoadControl implements Controller {
 
 		if (vo == null) {
 			session.setAttribute("error", "로그인이 필요합니다. 로그인 페이지로 이동합니다.");
-			response.sendRedirect("login.do");
+			response.sendRedirect("view/user/login.tiles");
 			return;
 
 			
